031d006e64ac4c9c1bc70fc734f239d58c3e32de
priyanka prasad <59612128+P-riyanka-prasad@users.noreply.github.com>
2020-06-06 13:30:39 +0530

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -1,4 +1,53 @@
-
+# Solution to find the is BST Satisfied or not with Helper method
 def is_bst_satisfied(self):
-  pass
-# write your code here
+    def helper(node, lower=float('-inf'), upper=float('inf')):
+        if not node:
+            return True
+            
+        val = node.data
+        if val <= lower or val >= upper:
+            return False
+
+        if not helper(node.right, val, upper):
+            return False
+        if not helper(node.left, lower, val):
+            return False
+        return True
+
+    return helper(self.root)
+  
+  # solution with Stack 
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def is_BST(root):
+    stack = []
+    prev = None
+    
+    while root or stack:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        if prev and root.val <= prev.val:
+            return False
+        prev = root
+        root = root.right
+    return True
+
+root = TreeNode(2)  
+root.left = TreeNode(1)  
+root.right = TreeNode(3) 
+ 
+result = is_BST(root)
+print(result)
+
+root = TreeNode(1)  
+root.left = TreeNode(2)  
+root.right = TreeNode(3) 
+ 
+result = is_BST(root)
+print(result)
